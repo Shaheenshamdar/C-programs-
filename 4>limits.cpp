820aa6f2b69e59da3230de541c731d5f71324326
Shaheenshamdar <shaheenshamdar@gmail.com>
2025-07-13 22:18:09 +0530

    Create 4>limits.cpp

---
+++ 4>limits.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    cout << "Minimum int: " << INT_MIN << endl;
+    cout << "Maximum int: " << INT_MAX << endl;
+    return 0;
+}
