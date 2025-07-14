715bef9b012ba7a2e775676e4a04a91d135b7884
Shaheenshamdar <shaheenshamdar@gmail.com>
2025-07-14 08:56:08 +0530

    Create 11>pattern.cpp

---
+++ 11>pattern.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++)
+        cout << "x";
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+        cout << "x ";
+    cout << endl;
+
+    for (int i = 0; i < 3; i++)
+        cout << "x ";
+    cout << endl;
+
+    cout << "x ";
+    for (int i = 0; i < 7; i++)
+        cout << "x";
+    cout << " ";
+    for (int i = 0; i < 7; i++)
+        cout << "x";
+    cout << endl;
+
+    for (int i = 0; i < 3; i++)
+        cout << "x ";
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+        cout << "x ";
+    cout << endl;
+
+    for (int i = 0; i < 5; i++)
+        cout << "x";
+    cout << endl;
+
+    return 0;
+}
