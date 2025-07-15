a2c0b9edd47f5a5a25b19e631fadaeb58a867175
Shaheenshamdar <shaheenshamdar@gmail.com>
2025-07-15 09:15:47 +0530

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -1,9 +1,9 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
-
+namespace calculator {
 int add(int c, int d);
 int subtract(int c, int d);
 int multiply(int c, int d);
 int divide(int c, int d);
-
+}
 #endif
